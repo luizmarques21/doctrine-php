1af7601ab088f7359a2a4fe3c02443eca6e873a8
Luiz Marques <mariel.marques21@gmail.com>
2024-09-22 00:09:13 -0300

    Aula 5: Relacionamento ManyToMany

--- src/Entity/Student.php
+++ src/Entity/Student.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\GeneratedValue;
 use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\ManyToMany;
 use Doctrine\ORM\Mapping\OneToMany;
 
 #[Entity]
@@ -22,11 +23,15 @@ class Student
 		cascade: ["persist", "remove"])]
 	private Collection $phones;
 
+	#[ManyToMany(targetEntity: Course::class, inversedBy: "students")]
+	private Collection $courses;
+
 	public function __construct(
 		#[Column]
 		public readonly string $name
 	) {
 		$this->phones = new ArrayCollection();
+		$this->courses = new ArrayCollection();
 	}
 
 	public function addPhone(Phone $phone) {
@@ -39,6 +44,23 @@ class Student
 	 */
 	public function phones(): Collection {
 		return $this->phones;
+	}
+
+	/**
+	 * @return Collection<Course>
+	 */
+	public function courses(): Collection {
+		return $this->courses;
+	}
+
+	public function enrollInCourse(Course $course): void
+	{
+		if ($this->courses->contains($course)) {
+			return;
+		}
+
+		$this->courses->add($course);
+		$course->addStudent($this);
 	}
 
 }
